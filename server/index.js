6be13e8b89b51ed4d140a5286dc15fcc3bde7f96
tuanlam1610 <73232399+tuanlam1610@users.noreply.github.com>
2023-03-30 23:37:48 +0700

    Add API for dashboard and API to add data to database

--- server/index.js
+++ server/index.js
@@ -3,6 +3,15 @@ const app = express()
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+
+async function hehe() {
+  const hashedPassword = await bcrypt.hash('myPassword', 10);
+  // Compare a password with a hash
+  const result = await bcrypt.compare('myPassword', hashedPassword);
+  console.log(result);
+}
+
 require('dotenv/config');
 const PORT = 5000
 
@@ -14,6 +23,21 @@ app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello world.');
+})
+
+app.post('/user/add', async (req, res) => {
+  try {
+    req.body.password = await bcrypt.hash(req.body.password, 10);
+    const newUser = new model.User(req.body);
+    const savedUser = await newUser.save();
+    res.status(200).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ success: false, msg: err.message });
+  }
+})
+
+app.post('/login', async (req, res) => {
+
 })
 
 app.post('/category/add', async (req, res) => {
@@ -47,6 +71,61 @@ app.post('/book/add', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
+})
+
+app.post('/order/add', async (req, res) => {
+
+  try {
+    // Tính tổng giá trị đơn hàng
+    var total = 0;
+    for (i in req.body.listOfBook) {
+      const price = (await model.Book.findById(req.body.listOfBook[i].book)).price;
+      console.log(price)
+      total += req.body.listOfBook[i].quantity * price;
+    }
+    console.log(total);
+    //Tạo đối tượng đơn hàng
+    const order = new model.Order({
+      listOfBook: req.body.listOfBook,
+      totalPrice: total
+    });
+
+    // Lưu đơn hàng vào database
+    await order.save();
+
+    // Trả về kết quả thành công
+    res.status(201).json(order);
+  } catch (error) {
+    // Trả về thông báo lỗi nếu có lỗi xảy ra
+    res.status(500).json({ error: error.message });
+  }
+})
+
+app.get('/dashboard', async (req, res) => {
+  const numOfBooks = await model.Book.count()
+  const numOfOrderPerWeek = await model.Order.find().where({ date: "2023-03-30T15:15:08.402Z" });
+
+  const startOfWeek = new Date();
+  startOfWeek.setHours(startOfWeek.getHours() - 2);
+
+  const endOfWeek = new Date();
+  endOfWeek.setHours(endOfWeek.getHours() - 1);
+
+  const count = await model.Order.find({
+    date: {
+      $gte: startOfWeek,
+      $lte: endOfWeek
+    }
+  });
+
+  const listOfBookOutOfStock = await model.Book.find({
+    stock:{
+      $lte: 5
+    }
+  }).limit(5);
+
+  console.log(listOfBookOutOfStock);
+  res.status(200).json(listOfBookOutOfStock)
 })
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
